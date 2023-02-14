ea02b7b1fd459ece05198df86b6fcd4b09ab4a32
nawimnate <jnhatch3@gmail.com>
2023-02-14 13:06:38 -0500

    simple footer example

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-section">
+      <hr />
+      <p>JNH LLC © 2023 by hatchbase. All Rights Reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
